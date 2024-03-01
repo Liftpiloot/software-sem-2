0a2e0f3c1dff9302809db201e7f8300c42e1ad3b
Abel <vandijkabel@gmail.com>
2024-03-01 15:05:43 +0100

    Add html css for login

--- IndividueelProject - Iron/IronApp/IronApp/Controllers/LoginController.cs
+++ IndividueelProject - Iron/IronApp/IronApp/Controllers/LoginController.cs
@@ -51,7 +51,7 @@ public class LoginController : Controller
         }
         else
         {
-            return View();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
