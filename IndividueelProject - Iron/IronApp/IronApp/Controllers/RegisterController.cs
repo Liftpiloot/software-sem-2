bc98da8cd413d9bfecd2311dd3b48113190142b8
Abel <vandijkabel@gmail.com>
2024-03-07 13:44:27 +0100

    Add check for email/username already in use

--- IndividueelProject - Iron/IronApp/IronApp/Controllers/RegisterController.cs
+++ IndividueelProject - Iron/IronApp/IronApp/Controllers/RegisterController.cs
@@ -9,12 +9,13 @@ namespace IronApp.Controllers;
 public class RegisterController : Controller
 {
     private string _db = "Server=localhost\\SQLEXPRESS;Database=iron;Trusted_Connection=True;Encrypt=False;";
-    
+
     // GET
     public IActionResult Index()
     {
         return View();
     }
+
     // POST
     [HttpPost]
     public IActionResult Index(RegisterModel model)
@@ -22,6 +23,20 @@ public class RegisterController : Controller
         Console.WriteLine(model.DateOfBirth);
         if (ModelState.IsValid)
         {
+            // check if username or email already exists
+            SqlConnection conn = new SqlConnection(_db);
+            conn.Open();
+            SqlCommand cmd =
+                new SqlCommand("SELECT * FROM users WHERE username = @username OR LOWER(email) = LOWER(@email)", conn);
+            cmd.Parameters.AddWithValue("@username", model.Username);
+            cmd.Parameters.AddWithValue("@email", model.Email);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                ModelState.AddModelError("Username", "Username or email already exists.");
+                return View(model);
+            }
+
             // Create sha256 hash
             using (SHA256 sha256Hash = SHA256.Create())
             {
@@ -31,17 +46,32 @@ public class RegisterController : Controller
                 {
                     builder.Append(bytes[i].ToString("x2"));
                 }
+
                 model.Password = builder.ToString();
             }
-            SqlConnection conn = new SqlConnection(_db);
+
+            conn = new SqlConnection(_db);
             conn.Open();
-            SqlCommand cmd = new SqlCommand("INSERT INTO users (username, passwordhash, email, dateofbirth, weight) VALUES (@username, @password, @email, @dateOfBirth, @weight)", conn);
+            cmd = new SqlCommand(
+                "INSERT INTO users (username, passwordhash, email, dateofbirth, weight) OUTPUT INSERTED.id VALUES (@username, @password, @email, @dateOfBirth, @weight)",
+                conn);
             cmd.Parameters.AddWithValue("@username", model.Username);
             cmd.Parameters.AddWithValue("@password", model.Password);
             cmd.Parameters.AddWithValue("@email", model.Email);
             cmd.Parameters.AddWithValue("@dateOfBirth", model.DateOfBirth);
             cmd.Parameters.AddWithValue("@weight", model.Weight);
-            cmd.ExecuteNonQuery();
+            int id = (int) cmd.ExecuteScalar();
+            conn.Close();
+            // Create cookie with user id
+            var cookieOptions = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(365), // Cookie expires after a year
+                IsEssential = true
+            };
+            Response.Cookies.Append("UserId", id.ToString() ?? throw new InvalidOperationException(), cookieOptions);
+            TempData["Id"] = id;
+            TempData["Username"] = model.Username;
+            
             return RedirectToAction("Index", "Home");
         }
         else
@@ -50,14 +80,17 @@ public class RegisterController : Controller
             {
                 ModelState.AddModelError("Password", "Password is required.");
             }
+
             if (model.Password != model.ConfirmPassword)
             {
                 ModelState.AddModelError("ConfirmPassword", "Password and confirmation password do not match.");
             }
+
             if (model.Password.Length < 10)
             {
                 ModelState.AddModelError("Password", "The password must be at least 10 characters long.");
             }
+
             if (model.DateOfBirth == null)
             {
                 ModelState.AddModelError("DateOfBirth", "Date of birth is not required.");
@@ -70,13 +103,13 @@ public class RegisterController : Controller
                     ModelState.AddModelError("DateOfBirth", "Invalid date format.");
                 }
             }
+
             if (model.Weight <= 0)
             {
                 ModelState.AddModelError("Weight", "Weight is required.");
             }
+
             return View(model);
         }
-        
     }
-
 }
