9f5bb4a46b17821946cbba1fca7b1c5ec0e3872c
Abel <vandijkabel@gmail.com>
2024-03-11 16:42:36 +0100

    More homepage styling

--- IndividueelProject - Iron/IronApp/IronApp/Controllers/HomeController.cs
+++ IndividueelProject - Iron/IronApp/IronApp/Controllers/HomeController.cs
@@ -35,13 +35,7 @@ public class HomeController : Controller
                 ViewBag.Username = reader["username"].ToString() ?? "No username";
             }
         }
-        List<string> exercises = new List<string>();
-        exercises.Add("Bench Press");
-        exercises.Add("Squat");
-        exercises.Add("Deadlift");
-        exercises.Add("Pull-up");
-        exercises.Add("Dumbbell Curl");
-        return View(exercises);
+        return View(GetExercises());
     }
 
     public IActionResult Privacy()
@@ -54,5 +48,15 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public List<ExerciseModel> GetExercises()
+    {
+        List<ExerciseModel> exercises = new List<ExerciseModel>();
+        // fake list of exercises
+        exercises.Add(new ExerciseModel { Name = "Bench Press", Description = "Lay on a bench and press the bar", logo= "images/bench_wireframe.png" });
+        exercises.Add(new ExerciseModel { Name = "Bicep Curl", Description = "Hold a dumbell, with one hand and lift it up engaging your bicep", logo = "images/bicep_curl_wireframe.png" });
+        exercises.Add(new ExerciseModel { Name = "Lat Pulldown", Description = "", logo = "images/lat_pulldown_wireframe.png" });
+        return exercises;
     }
 }
