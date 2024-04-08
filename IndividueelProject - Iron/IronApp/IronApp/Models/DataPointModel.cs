422d5f04d36108c233166edf0eb89e7be178d6c0
Abel <vandijkabel@gmail.com>
2024-04-08 13:58:21 +0200

    Add datapoint model

---
+++ IndividueelProject - Iron/IronApp/IronApp/Models/DataPointModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace IronApp.Models;
+
+public class DataPointModel
+{
+    //DataContract for Serializing Data - required to serve in JSON format
+    [DataContract]
+    public class DataPoint
+    {
+        public DataPoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+ 
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "x")]
+        public double? X = null;
+ 
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public double? Y = null;
+    }
+}
