030671ae16b5be25844e44eae99cfd12b01d444d
Abel <vandijkabel@gmail.com>
2024-04-15 09:18:58 +0200

    Moved files and added gitignore

--- IndividueelProject-Iron/IronApp/IronApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace IronApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- IndividueelProject-Iron/IronApp/IronApp/Models/SetModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IronApp.Models;
-
-public class SetModel
-{
-    public decimal Weight { get; set; }
-    public int Reps { get; set; }
-    public DateTime Date { get; set; }
-}
