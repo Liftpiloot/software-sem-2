7cd9bee4fab67ac871660834e7e39f3e666fc636
Abel <vandijkabel@gmail.com>
2024-05-06 13:37:01 +0200

    Add Exercise unit tests

---
+++ IndividueelProject/TestProject/DbExerciseTest.cs
@@ -0,0 +1,122 @@
+﻿using Iron_DAL.DTO;
+using Iron_Interface;
+
+namespace TestProject;
+
+public class DbExerciseTest : IDbExercise
+{
+    public int AddExercise(ExerciseDto exercise)
+    {
+        return 1;
+    }
+
+    public ExerciseDto? GetExercise(ExerciseDto exercise)
+    {
+        if (exercise.Id != 0)
+        {
+            return new ExerciseDto
+            {
+                Id = exercise.Id,
+                UserId = 1,
+                Name = "Test",
+                Description = "Test",
+                Logo = "/images/default.png"
+            };
+        }
+        return null;
+    }
+
+    public bool AddSelectedExercise(SelectedExerciseDto selectedExerciseDto)
+    {
+        if (selectedExerciseDto.ExerciseId != 0)
+        {
+            if (selectedExerciseDto.UserId != 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool DeleteSelectedExercise(SelectedExerciseDto selectedExerciseDto)
+    {
+        if (selectedExerciseDto.ExerciseId != 0)
+        {
+            if (selectedExerciseDto.UserId != 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<SelectedExerciseDto> GetSelectedExercises(UserDto user)
+    {
+        List<SelectedExerciseDto> selectedExercises = new()
+        {
+            new SelectedExerciseDto
+            {
+                UserId = user.Id,
+                ExerciseId = 1
+            },
+            new SelectedExerciseDto
+            {
+                UserId = user.Id,
+                ExerciseId = 2
+            }
+        };
+        return selectedExercises;
+    }
+
+    public List<ExerciseDto> GetUnselectedExercises(UserDto user)
+    {
+        List<ExerciseDto> exercises = new()
+        {
+            new ExerciseDto
+            {
+                Id = 1,
+                UserId = user.Id,
+                Name = "Test",
+                Description = "Test",
+                Logo = "/images/default.png"
+            },
+            new ExerciseDto
+            {
+                Id = 2,
+                UserId = user.Id,
+                Name = "Test",
+                Description = "Test",
+                Logo = "/images/default.png"
+            }
+        };
+        return exercises;
+    }
+
+    public List<ExerciseDto> GetExercises(UserDto user)
+    {
+        List<ExerciseDto> exercises = new()
+        {
+            new ExerciseDto
+            {
+                Id = 1,
+                UserId = user.Id,
+                Name = "Test",
+                Description = "Test",
+                Logo = "/images/default.png"
+            },
+        };
+        return exercises;
+    }
+
+    public bool DeleteExercise(UserDto userDto, ExerciseDto exerciseDto)
+    {
+        if (exerciseDto.Id != 0)
+        {
+            if (userDto.Id != 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ IndividueelProject/TestProject/UnitTestExercise.cs
@@ -0,0 +1,109 @@
+﻿using Iron_DAL.DTO;
+using Iron_Domain;
+using Iron_Interface;
+using IronDomain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject;
+
+[TestClass]
+public class UnitTestExercise
+{
+    static IDbExercise db = new DbExerciseTest();
+    ExerciseContainer exerciseContainer = new(db);
+    
+    [TestMethod]
+    public void AddExercise()
+    {
+        Exercise exercise = new()
+        {
+            Name = "Test",
+            UserId = 1,
+            Description = "Test",
+            Logo = "/images/default.png"
+        };
+        int id = exerciseContainer.AddExercise(exercise);
+        Assert.AreEqual(1, id);
+    }
+    
+    [TestMethod]
+    public void DeleteExercise()
+    {
+        User user = new()
+        {
+            Id = 1
+        };
+        Exercise exercise = new()
+        {
+            Id = 1
+        };
+        bool deleted = exerciseContainer.DeleteExercise(user, exercise);
+        Assert.IsTrue(deleted);
+    }
+    
+    [TestMethod]
+    public void GetExerciseFromId()
+    {
+        Exercise? exercise = exerciseContainer.GetExerciseFromId(1);
+        Assert.IsNotNull(exercise);
+    }
+    
+    [TestMethod]
+    public void GetExercises()
+    {
+        User user = new()
+        {
+            Id = 1
+        };
+        List<Exercise> exercises = exerciseContainer.GetExercises(user);
+        Assert.IsNotNull(exercises);
+    }
+    
+    [TestMethod]
+    public void AddSelectedExercise()
+    {
+        SelectedExercise selectedExercise = new()
+        {
+            UserId = 1,
+            ExerciseId = 1
+        };
+        bool added = exerciseContainer.AddSelectedExercise(selectedExercise);
+        Assert.IsTrue(added);
+    }
+    
+    [TestMethod]
+    public void DeleteSelectedExercise()
+    {
+        SelectedExercise selectedExercise = new()
+        {
+            UserId = 1,
+            ExerciseId = 1
+        };
+        bool deleted = exerciseContainer.DeleteSelectedExercise(selectedExercise);
+        Assert.IsFalse(deleted);
+    }
+    
+    [TestMethod]
+    public void GetSelectedExercises()
+    {
+        User user = new()
+        {
+            Id = 1
+        };
+        List<SelectedExercise> exercises = exerciseContainer.GetSelectedExercises(user);
+        Assert.IsNotNull(exercises);
+        Assert.AreEqual(user.Id, exercises[0].UserId);
+    }
+
+    [TestMethod]
+    public void GetUnselectedExercises()
+    {
+        User user = new()
+        {
+            Id = 1
+        };
+        List<Exercise> exercises = exerciseContainer.GetUnselectedExercises(user);
+        Assert.IsNotNull(exercises);
+        Assert.AreEqual(user.Id, exercises[0].UserId);
+    }
+}
