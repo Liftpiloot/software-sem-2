3196bdf7f17ebb8ab74c1ffc19b3fb794c546a94
Abel <vandijkabel@gmail.com>
2024-05-28 10:48:18 +0200

    Add unit tests for magazijn

---
+++ Algoritmiek/Magazijn/MagazijnOpdracht/MagazijnTest/UnitTestCloset.cs
@@ -0,0 +1,61 @@
+using MagazijnOpdracht;
+
+namespace MagazijnTest;
+
+public class Tests
+{
+    private Closet _closet;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _closet = new Closet();
+    }
+    
+    [Test]
+    public void Init_ShouldCreateClosetWithOneShelf()
+    {
+        // Arrange
+        var closet = new Closet();
+        
+        // Act
+        var result = closet.Shelves.Count;
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+        Assert.That(closet.Shelves[0].Height, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void AddShelf_ShouldAddShelfToCloset()
+    {
+        // Arrange
+        var shelf = new Shelf(2);
+
+        // Act
+        var result = _closet.AddShelf(shelf);
+
+        // Assert
+        Assert.That(result, Is.True);
+        Assert.That(_closet.Shelves.Count, Is.EqualTo(2));
+        Assert.That(_closet.Shelves[0], Is.EqualTo(shelf));
+    }
+
+    [Test]
+    public void TopShelf_ShouldReturnTopShelf()
+    {
+        // Arrange
+        var shelf1 = new Shelf(1);
+        var shelf2 = new Shelf(2);
+        _closet.AddShelf(shelf1);
+        _closet.AddShelf(shelf2);
+
+        // Act
+        var result = _closet.TopShelf();
+
+        // Assert
+        Assert.AreEqual(shelf2, result);
+    }
+    
+    
+}
---
+++ Algoritmiek/Magazijn/MagazijnOpdracht/MagazijnTest/UnitTestShelf.cs
@@ -0,0 +1,85 @@
+﻿using MagazijnOpdracht;
+
+namespace MagazijnTest;
+
+public class UnitTestShelf
+{
+    private Shelf _shelf;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _shelf = new Shelf(1);
+    }
+    
+    [Test]
+    public void Init_ShouldCreateShelfWithHeightAndEmptyProductsList()
+    {
+        // Arrange
+        var shelf = new Shelf(1);
+        
+        // Act
+        var result = shelf.Height;
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+        Assert.That(shelf.Products.Count, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void AddProduct_ShouldAddProductToShelf()
+    {
+        // Arrange
+        var product = new Product { Size = Size.Small, Speed = Speed.Fast };
+
+        // Act
+        var result = _shelf.AddProduct(product);
+
+        // Assert
+        Assert.IsTrue(result);
+        Assert.AreEqual(1, _shelf.Products.Count);
+    }
+
+    [Test]
+    public void IsEmpty_ShouldReturnTrueWhenShelfIsEmpty()
+    {
+        // Assert
+        Assert.IsTrue(_shelf.IsEmpty());
+    }
+
+    [Test]
+    public void IsFull_ShouldReturnFalseWhenShelfIsNotFull()
+    {
+        // Assert
+        Assert.IsFalse(_shelf.IsFull());
+    }
+
+    [Test]
+    public void AddProduct_ShouldNotAddLargeProductToNonFirstOrFifthShelf()
+    {
+        // Arrange
+        _shelf = new Shelf(2);
+        var product = new Product { Size = Size.Large, Speed = Speed.Fast };
+
+        // Act
+        var result = _shelf.AddProduct(product);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void AddProduct_ShouldNotAddMediumProductToEighthShelf()
+    {
+        // Arrange
+        _shelf = new Shelf(8);
+        var product = new Product { Size = Size.Medium, Speed = Speed.Fast };
+
+        // Act
+        var result = _shelf.AddProduct(product);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+    
+}
